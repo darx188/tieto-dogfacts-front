1132db51cdeb855dbca5370aad77b2239f108214
darx188 <darius.palevic@gmail.com>
2020-05-28 14:06:54 +0300

    added readme

--- src/factDataService.js
+++ src/factDataService.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const FACT_API_URL = 'https://shrouded-reef-71173.herokuapp.com'
+const FACT_API_URL = 'localhost:8080'
 class factDataService {
     retrieveAllFacts() {
         return axios.get(`${FACT_API_URL}/facts`);
